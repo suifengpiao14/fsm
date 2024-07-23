dcbb364c40543b954040d468ee6917883180018d
zheng peng <pengzheng@huishoubao.com.cn>
2024-07-23 17:56:09 +0800

    fix

--- fsm_ext.go
+++ fsm_ext.go
@@ -12,7 +12,20 @@ func (f *FSM) EventName(dst string) (eventName string, ok bool) {
 	return "", false
 }
 
-// AvailableDstStates return true if it is a reverse order event(the dest status in src status)
+// AvailableDstStates return all availabe dst state
+func (f *FSM) AvailableDstStates() (states []string) {
+	f.stateMu.RLock()
+	defer f.stateMu.RUnlock()
+	states = make([]string, 0)
+	for key, transition := range f.transitions {
+		if key.src == f.current {
+			states = append(states, transition)
+		}
+	}
+	return states
+}
+
+// IsReverseOrder return true if it is a reverse order event(the dest status in src status)
 func (f *FSM) IsReverseOrder(beforStatus string) (ok bool) {
 	f.stateMu.RLock()
 	defer f.stateMu.RUnlock()
